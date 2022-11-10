0507ba11c730e324b079d9920acd3739ad3ebd0d
huyphamfc <huyphamfc@gmail.com>
2022-11-10 12:17:51 +0700

    rebuild HeroSection layout

--- client/src/layouts/HeroSection/index.js
+++ client/src/layouts/HeroSection/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import style from './style.module.scss';
 import { Button } from '../../components';
+import image from '../../assets/images/hero-illustration.svg';
 
 function HeroSection() {
   return (
-    <section className={style.carousel}>
+    <section className={style.hero}>
       <div className={style.container}>
         <div className={style.text}>
           <h2 className={style.heading}>English for IT</h2>
@@ -18,7 +19,9 @@ function HeroSection() {
             <Button outline>Learn more &darr;</Button>
           </div>
         </div>
-        <div className={style.img} />
+        <div className={style.img}>
+          <img src={image} alt="The Earth." />
+        </div>
       </div>
     </section>
   );
