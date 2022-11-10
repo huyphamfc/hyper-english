242aa9882b91fe05fc74775d11287cedaa6ec046
huyphamfc <huyphamfc@gmail.com>
2022-11-10 21:33:05 +0700

    create Products layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-/* eslint-disable object-curly-newline */
 import React from 'react';
 
 import { GlobalStyles } from './components';
-import { Header, HeroSection, About, Mission, Testimonial } from './layouts';
+import {
+  Header,
+  HeroSection,
+  About,
+  Mission,
+  Products,
+  Testimonial,
+} from './layouts';
 
 function App() {
   return (
@@ -11,6 +17,7 @@ function App() {
       <HeroSection />
       <About />
       <Mission />
+      <Products />
       <Testimonial />
     </GlobalStyles>
   );
--- client/src/layouts/index.js
+++ client/src/layouts/index.js
@@ -2,4 +2,5 @@ export { default as Header } from './Header';
 export { default as HeroSection } from './HeroSection';
 export { default as About } from './About';
 export { default as Mission } from './Mission';
+export { default as Products } from './Products';
 export { default as Testimonial } from './Testimonial';
