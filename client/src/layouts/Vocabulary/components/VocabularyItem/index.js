09817697155d1c013f43832fb62bab17a56761b0
huyphamfc <huyphamfc@gmail.com>
2022-11-16 09:06:35 +0700

    create VocabularyItem component

---
+++ client/src/layouts/Vocabulary/components/VocabularyItem/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+
+import style from './style.module.scss';
+
+// prettier-ignore
+function VocabularyItem({
+  vocabulary,
+  pronunciation,
+  meaning,
+  example,
+}) {
+  return (
+    <ul className={style.item}>
+      <li className={style.vocabulary}>{vocabulary}</li>
+      <li className={style.pronunciation}>
+        <span>
+          <FontAwesomeIcon icon={faVolumeHigh} />
+          &nbsp;
+        </span>
+        <span>{pronunciation}</span>
+      </li>
+      <li className={style.meaning}>{meaning}</li>
+      <li className={style.example}>{example}</li>
+    </ul>
+  );
+}
+
+VocabularyItem.propTypes = {
+  vocabulary: PropTypes.string.isRequired,
+  pronunciation: PropTypes.string.isRequired,
+  meaning: PropTypes.string.isRequired,
+  example: PropTypes.string.isRequired,
+};
+
+export default VocabularyItem;
