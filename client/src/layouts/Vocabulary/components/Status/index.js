c55d23401773eed9e1e68d3095a2d6abc37808a8
huyphamfc <huyphamfc@gmail.com>
2022-11-16 09:05:53 +0700

    create Status component

---
+++ client/src/layouts/Vocabulary/components/Status/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+import style from './style.module.scss';
+import { Button } from '../../../../components';
+
+function Status({ process }) {
+  return (
+    <div className={style.status}>
+      <div className={style.process}>
+        <div
+          className={style.process__inner}
+          style={{ width: `${process}%` }}
+        />
+      </div>
+      <Button className={style.exit} transparent>
+        <FontAwesomeIcon icon={faXmark} />
+      </Button>
+    </div>
+  );
+}
+
+Status.propTypes = {
+  process: PropTypes.number.isRequired,
+};
+
+export default Status;
