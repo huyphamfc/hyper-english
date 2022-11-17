b9e18d69f753c23f9a5b25ccbf3375eec5d6d043
huyphamfc <huyphamfc@gmail.com>
2022-11-17 16:17:48 +0700

    edit BackToTop button

--- client/src/layouts/BackToTop/components/BackToTopButton/index.js
+++ client/src/layouts/BackToTop/components/BackToTopButton/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 import style from './style.module.scss';
-import Button from '../Button/Button';
+import Button from '../../../../components/Button/Button';
 
 function BackToTop() {
-  const { showBackToTop } = useSelector((state) => state.backToTop);
-
-  const btnClass = classNames(style.button, {
-    [style.show]: showBackToTop,
-  });
-
   const handleBackToTop = () => {
     window.scrollTo({
       top: 0,
@@ -23,7 +15,7 @@ function BackToTop() {
   };
 
   return (
-    <Button className={btnClass} onClick={handleBackToTop}>
+    <Button className={style.button} onClick={handleBackToTop}>
       <FontAwesomeIcon icon={faArrowUp} />
     </Button>
   );
