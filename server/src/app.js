35d8b94d61391b209b99a404e3953f0b00aa35e2
huyphamfc <huyphamfc@gmail.com>
2022-11-18 10:12:59 +0700

    connect MongoDB

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const express = require('express');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express());
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+app.get('/api', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Hello from the server!',
+  });
+});
+
+module.exports = app;
