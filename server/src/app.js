c01fe7275a7b9a0611288a0b0bdd2f0783949230
huyphamfc <huyphamfc@gmail.com>
2022-11-19 08:46:05 +0700

    handle Unhandled Routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,14 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const express = require('express');
 
+const AppError = require('./utils/AppError');
+const handleGlobalError = require('./controllers/globalErrorController');
+
 dotenv.config();
 
 const app = express();
 
-app.use(express());
+app.use(express.json());
 
 app.use('/api/public', express.static('public'));
 
@@ -18,5 +21,11 @@ app.get('/api', (req, res) => {
     message: 'Hello from the server!',
   });
 });
+
+app.all('*', (req, res, next) => {
+  next(new AppError(404, `Cannot find ${req.originalUrl} on the server.`));
+});
+
+app.use(handleGlobalError);
 
 module.exports = app;
