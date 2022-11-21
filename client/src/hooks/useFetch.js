30d4a29ea20fc1168fe844bb4371e624a5538c88
huyphamfc <huyphamfc@gmail.com>
2022-11-21 20:35:13 +0700

    create useFetch hook

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+function useFetch(param) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/${param}`)
+      .then((res) => res.json())
+      .then((result) => setData(result));
+  }, [param]);
+
+  return data;
+}
+
+export default useFetch;
