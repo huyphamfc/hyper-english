37a5665e450782aa4780d5a7a0cf783fc157466a
huyphamfc <huyphamfc@gmail.com>
2022-11-26 10:45:27 +0700

    create Login form validation

--- client/src/layouts/Authentication/components/Login/index.js
+++ client/src/layouts/Authentication/components/Login/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import style from './style.module.scss';
 import { Button } from '../../../../components';
 import FormGroup from '../FormGroup';
+import loginYupSchema from './loginYupSchema';
 
 function Login() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ mode: 'onTouched', resolver: yupResolver(loginYupSchema) });
+
+  const submitForm = (payload, e) => {
+    console.log(payload);
+    e.target.reset();
+  };
+
   return (
     <>
-      <form className={style.form}>
-        <FormGroup inputPlaceholder="Email" />
-        <FormGroup inputPlaceholder="Password" inputType="password" />
+      <form className={style.form} onSubmit={handleSubmit(submitForm)}>
+        <FormGroup
+          inputProps={register('email')}
+          inputWarning={errors.email?.message}
+          inputPlaceholder="Email"
+        />
+        <FormGroup
+          inputProps={register('password')}
+          inputWarning={errors.password?.message}
+          inputPlaceholder="Password"
+          inputType="password"
+        />
         <Button solid>Login</Button>
       </form>
       <div className={style.action}>
