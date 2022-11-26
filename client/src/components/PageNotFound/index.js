142036bb23f7a76a4340c252f7acb3514493f102
huyphamfc <huyphamfc@gmail.com>
2022-11-26 13:56:56 +0700

    restructure PageNotFound component

--- client/src/components/PageNotFound/index.js
+++ client/src/components/PageNotFound/index.js
@@ -1,21 +1,3 @@
-import React from 'react';
-
-import style from './style.module.scss';
-import img from '../../assets/icons/page-not-found.svg';
-import Button from '../Button';
-
-function PageNotFound() {
-  return (
-    <section className={style.page}>
-      <div className={style.container}>
-        <img className={style.img} src={img} alt="Icon" />
-        <p>PAGE NOT FOUND</p>
-        <Button solid to="/">
-          Go Home
-        </Button>
-      </div>
-    </section>
-  );
-}
+import PageNotFound from './PageNotFound';
 
 export default PageNotFound;
