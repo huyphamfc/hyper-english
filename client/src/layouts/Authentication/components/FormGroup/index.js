6316949b94b6c36c780f64669c2c29d4c2f0ee9a
huyphamfc <huyphamfc@gmail.com>
2022-11-26 10:40:34 +0700

    style Error UI

--- client/src/layouts/Authentication/components/FormGroup/index.js
+++ client/src/layouts/Authentication/components/FormGroup/index.js
@@ -1,27 +1,50 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import style from './style.module.scss';
 
-function FormGroup({ inputPlaceholder, inputType }) {
+// prettier-ignore
+function FormGroup({
+  inputProps,
+  inputWarning,
+  inputPlaceholder,
+  inputType,
+}) {
+  const inputClass = classNames(style.input, {
+    [style['input--error']]: inputWarning,
+  });
+
+  const warningClass = classNames(style.warning, {
+    [style['warning--error']]: inputWarning,
+  });
+
   return (
     <div>
       <input
-        className={style.input}
+        {...inputProps}
+        className={inputClass}
         placeholder={inputPlaceholder}
         type={inputType}
       />
-      <span className={style.warning}>Warning</span>
+      <span className={warningClass}>
+        {inputWarning || 'Error placeholder'}
+      </span>
     </div>
   );
 }
 
 FormGroup.propTypes = {
+  inputProps: PropTypes.object.isRequired,
+  inputWarning: PropTypes.string,
   inputPlaceholder: PropTypes.string,
   inputType: PropTypes.string,
 };
 
 FormGroup.defaultProps = {
+  inputWarning: null,
   inputPlaceholder: null,
   inputType: 'text',
 };
