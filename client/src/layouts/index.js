81a5b8ee8f3a21a31794169b0232ffc4fa376314
huyphamfc <huyphamfc@gmail.com>
2022-11-26 14:19:25 +0700

    restructure layouts folder

--- client/src/layouts/index.js
+++ client/src/layouts/index.js
@@ -1,14 +1,14 @@
 export { default as Header } from './Header';
-export { default as HeroSection } from './HeroSection';
+export { default as HeroSection } from './HeroSection/HeroSection';
 export { default as About } from './About';
 export { default as Missions } from './Missions';
 export { default as Products } from './Products';
 export { default as Testimonials } from './Testimonials';
-export { default as Application } from './Application';
-export { default as Features } from './Features';
+export { default as Application } from './Application/Application';
+export { default as Features } from './Features/Features';
 export { default as Footer } from './Footer';
 export { default as BackToTop } from './BackToTop';
 
-export { default as Authentication } from './Authentication';
+export { default as Auth } from './Auth';
 export { default as Lessons } from './Lessons';
 export { default as Vocabulary } from './Vocabulary';
