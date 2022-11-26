6a05dfb3be9206c94bd3f04d7bcff9246a88a0e1
huyphamfc <huyphamfc@gmail.com>
2022-11-26 10:43:25 +0700

    create Login Yup Schema

---
+++ client/src/layouts/Authentication/components/Login/loginYupSchema.js
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+const yupSchema = yup.object({
+  email: yup
+    .string()
+    .required('Please enter your email.')
+    .email('Please enter a valid email.'),
+
+  password: yup.string().required('Please enter your password.'),
+});
+
+export default yupSchema;
