e139d44a0d7e1948e73f231b8c3b261ee71fb6d3
huyphamfc <huyphamfc@gmail.com>
2022-11-26 10:41:31 +0700

    create Signup Yup Schema

---
+++ client/src/layouts/Authentication/components/Signup/signupYupSchema.js
@@ -0,0 +1,22 @@
+import * as yup from 'yup';
+
+const yupSchema = yup.object({
+  name: yup.string().required('Please enter your name.'),
+
+  email: yup
+    .string()
+    .required('Please enter your email.')
+    .email('Please enter a valid email.'),
+
+  password: yup
+    .string()
+    .required('Please enter your password.')
+    .min(8, 'The password must be at least 8 characters.'),
+
+  passwordConfirmation: yup
+    .string()
+    .required('Please confirm your password.')
+    .oneOf([yup.ref('password')], 'The password confirmation does not match.'),
+});
+
+export default yupSchema;
