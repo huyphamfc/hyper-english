ae4081de9df3acfeb0ba86583b9bb90464a5390c
huyphamfc <huyphamfc@gmail.com>
2022-11-26 14:13:33 +0700

    restructure HeroSection layout

--- client/src/layouts/HeroSection/HeroSection.js
+++ client/src/layouts/HeroSection/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import style from './style.module.scss';
+import style from './HeroSection.module.scss';
 import { Button } from '../../components';
 import image from '../../assets/images/hero-illustration.svg';
 
