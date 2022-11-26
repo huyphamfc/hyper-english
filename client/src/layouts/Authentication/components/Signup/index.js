a18360800d1c69e87f5ff3869300a1bae65ecef4
huyphamfc <huyphamfc@gmail.com>
2022-11-26 10:44:39 +0700

    create Signup form validation

--- client/src/layouts/Authentication/components/Signup/index.js
+++ client/src/layouts/Authentication/components/Signup/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import style from './style.module.scss';
 import { Button } from '../../../../components';
 import FormGroup from '../FormGroup';
+import signupYupSchema from './signupYupSchema';
 
 function Signup() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ mode: 'onTouched', resolver: yupResolver(signupYupSchema) });
+
+  const submitForm = (payload, e) => {
+    console.log(payload);
+    e.target.reset();
+  };
+
   return (
     <>
-      <form className={style.form}>
-        <FormGroup inputPlaceholder="Name" />
-        <FormGroup inputPlaceholder="Email" />
-        <FormGroup inputPlaceholder="Password" inputType="password" />
+      <form className={style.form} onSubmit={handleSubmit(submitForm)}>
         <FormGroup
+          inputProps={register('name')}
+          inputWarning={errors.name?.message}
+          inputPlaceholder="Name"
+        />
+        <FormGroup
+          inputProps={register('email')}
+          inputWarning={errors.email?.message}
+          inputPlaceholder="Email"
+        />
+        <FormGroup
+          inputProps={register('password')}
+          inputWarning={errors.password?.message}
+          inputPlaceholder="Password"
+          inputType="password"
+        />
+        <FormGroup
+          inputProps={register('passwordConfirmation')}
+          inputWarning={errors.passwordConfirmation?.message}
           inputPlaceholder="Password Confirmation"
           inputType="password"
         />
