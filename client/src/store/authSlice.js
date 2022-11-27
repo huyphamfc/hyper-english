5dde3cbd9658cc20a9b996b57cc261390b524bcb
huyphamfc <huyphamfc@gmail.com>
2022-11-27 17:37:16 +0700

    create authSlice

---
+++ client/src/store/authSlice.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    isAuth: false,
+  },
+  reducers: {
+    login(state) {
+      state.isAuth = true;
+    },
+    logout(state) {
+      state.isAuth = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
