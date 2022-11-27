5131d57004f92252aadd878ce51a54f9fc8214e6
huyphamfc <huyphamfc@gmail.com>
2022-11-27 21:30:43 +0700

    fix authentication

--- client/src/layouts/Lessons/components/LessonsWrapper/LessonsWrapper.js
+++ client/src/layouts/Lessons/components/LessonsWrapper/LessonsWrapper.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router';
 
 import style from './LessonsWrapper.module.scss';
 import LessonItem from '../LessonItem';
 import data from '../../data';
-import { useAuth } from '../../../../hooks';
 
 function LessonsWrapper() {
-  useAuth();
+  const { isAuth } = useSelector((state) => state.auth);
+  const location = useLocation();
+  if (!isAuth) {
+    return <Navigate to="/auth/login" state={{ from: location }} replace />;
+  }
 
   const lessons = data.map((item) => (
     <LessonItem
