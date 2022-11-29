2dbb29d154d7537cf861565c79bf69b1cb625a8c
huyphamfc <huyphamfc@gmail.com>
2022-11-29 11:48:31 +0700

    create UserModel

---
+++ server/src/models/UserModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    select: false,
+  },
+  passwordUpdateAt: Date,
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordUpdateAt = Date.now() - 1000;
+  }
+
+  next();
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
