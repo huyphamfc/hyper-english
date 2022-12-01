7615f6d60416b48d4c75743950dd4397a4dd6e2b
huyphamfc <huyphamfc@gmail.com>
2022-12-01 09:33:59 +0700

    create sendErrDev method

--- server/src/controllers/globalErrorController.js
+++ server/src/controllers/globalErrorController.js
@@ -1,6 +1,26 @@
-module.exports = (err, req, res, next) => {
+const sendErrDev = (err, res) => {
+  console.error(err);
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  const processedErr = Object.assign(err);
+  processedErr.statusCode = processedErr.statusCode || 500;
+  processedErr.status = processedErr.status || 'error';
+  processedErr.message = processedErr.message || 'Something went wrong!';
+
+  const environment = process.env.NODE_ENV;
+
+  switch (environment) {
+    case 'development':
+      sendErrDev(processedErr, res);
+      break;
+
+    default:
+  }
 };
