2bc3c413aa5caacb0d95fb61053eef3fde849c91
huyphamfc <huyphamfc@gmail.com>
2022-12-02 20:33:48 +0700

    fetch Lessons data

--- client/src/layouts/Lessons/components/LessonsWrapper/LessonsWrapper.js
+++ client/src/layouts/Lessons/components/LessonsWrapper/LessonsWrapper.js
@@ -1,21 +1,23 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router';
 
 import style from './LessonsWrapper.module.scss';
 import LessonItem from '../LessonItem';
-import data from '../../data';
+import { useFetch } from '../../../../hooks';
 
 function LessonsWrapper() {
+  const { data } = useFetch('lessons');
   const { isAuth } = useSelector((state) => state.auth);
   const location = useLocation();
   if (!isAuth) {
     return <Navigate to="/" state={{ from: location }} replace />;
   }
 
-  const lessons = data.map((item) => (
+  const lessons = data?.map((item) => (
     <LessonItem
-      key={item.id}
+      key={item._id}
       lessonNumber={item.lessonNumber}
       name={item.name}
     />
