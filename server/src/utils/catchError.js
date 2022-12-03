bbee79abb38e3384423e11ae091e3fb567bd22e2
huyphamfc <huyphamfc@gmail.com>
2022-12-03 10:15:21 +0700

    create protectRoute method

--- server/src/utils/catchError.js
+++ server/src/utils/catchError.js
@@ -1,7 +1,7 @@
 function catchError(tryBlock) {
   return async function middlewareFunc(req, res, next) {
     try {
-      await tryBlock(req, res);
+      await tryBlock(req, res, next);
     } catch (err) {
       next(err);
     }
