461929d852eba1f878198db57b31460da4278b46
huyphamfc <huyphamfc@gmail.com>
2022-12-06 14:56:38 +0700

    create LessonsContent component

---
+++ client/src/layouts/Lessons/components/LessonsContent/LessonsContent.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+
+import style from './LessonsContent.module.scss';
+import LessonItem from '../LessonItem';
+import { useFetch } from '../../../../hooks';
+
+function LessonsContent() {
+  const { data } = useFetch('lessons');
+
+  const lessons = data?.map((item) => (
+    <LessonItem
+      key={item._id}
+      lessonNumber={item.lessonNumber}
+      name={item.name}
+    />
+  ));
+
+  return (
+    <section className={style.lessons}>
+      <ul className={style.items}>{lessons}</ul>
+    </section>
+  );
+}
+
+export default LessonsContent;
