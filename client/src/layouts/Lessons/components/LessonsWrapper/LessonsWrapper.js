e0a6399240cd9a1ef41ecc3bf8235f114ab6a74e
huyphamfc <huyphamfc@gmail.com>
2022-12-06 15:02:07 +0700

    protect Lessons route

--- client/src/layouts/Lessons/components/LessonsWrapper/LessonsWrapper.js
+++ client/src/layouts/Lessons/components/LessonsWrapper/LessonsWrapper.js
@@ -1,33 +1,14 @@
-/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router';
+import { Navigate } from 'react-router';
 
-import style from './LessonsWrapper.module.scss';
-import LessonItem from '../LessonItem';
-import { useFetch } from '../../../../hooks';
+import LessonsContent from '../LessonsContent';
 
 function LessonsWrapper() {
-  const { data } = useFetch('lessons');
   const { isAuth } = useSelector((state) => state.auth);
-  const location = useLocation();
-  if (!isAuth) {
-    return <Navigate to="/" state={{ from: location }} replace />;
-  }
 
-  const lessons = data?.map((item) => (
-    <LessonItem
-      key={item._id}
-      lessonNumber={item.lessonNumber}
-      name={item.name}
-    />
-  ));
-
-  return (
-    <section className={style.lessons}>
-      <ul className={style.items}>{lessons}</ul>
-    </section>
-  );
+  if (isAuth) return <LessonsContent />;
+  return <Navigate to="/" replace />;
 }
 
 export default LessonsWrapper;
