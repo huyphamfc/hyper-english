523f17932cf68cf88d85bec87ac6c2ae42924601
huyphamfc <huyphamfc@gmail.com>
2023-01-11 09:14:32 +0700

    create Lesson model

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,3 +1,4 @@
 export { default as AboutModel } from './about.model';
 export { default as MissionModel } from './mission.model';
 export { default as TestimonialModel } from './testimonial.model';
+export { default as LessonModel } from './lesson.model';
---
+++ server/src/models/lesson.model.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const lessonSchema = new mongoose.Schema({
+  lessonNumber: {
+    type: Number,
+    unique: true,
+  },
+  title: String,
+  bgColor: String,
+  imgSrc: String,
+  imgAlt: String,
+  vocabulary: {
+    type: [
+      {
+        word: String,
+        pronunciation: String,
+        meaning: String,
+        example: String,
+      },
+    ],
+    select: false,
+  },
+});
+
+const LessonModel = mongoose.model('Lesson', lessonSchema);
+
+export default LessonModel;
