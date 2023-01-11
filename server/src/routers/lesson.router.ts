4a3624b4b05bfa71a1229be124597b68603ba0f9
huyphamfc <huyphamfc@gmail.com>
2023-01-11 09:35:44 +0700

    protect Vocabulary data

--- server/src/routers/lesson.router.ts
+++ server/src/routers/lesson.router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { lessonController } from '../controllers';
+import { lessonController, authController } from '../controllers';
 
 const lessonRouter = express.Router();
 lessonRouter.route('/').get(lessonController.getAllLessons);
-lessonRouter.route('/:lessonNumber').get(lessonController.getVocabularyByLessonNumber);
+lessonRouter
+  .route('/:lessonNumber')
+  .get(authController.protectRoute, lessonController.getVocabularyByLessonNumber);
 
 export default lessonRouter;
