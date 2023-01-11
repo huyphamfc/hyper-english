634098f042c5fbd17829ad7a44f108ebdaafc6ea
huyphamfc <huyphamfc@gmail.com>
2023-01-11 09:33:17 +0700

    parse Cookie Header

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { aboutRouter, missionRouter, testimonialRouter, lessonRouter, userRouter } from './routers';
 import { handleUnhandledRoute, handleGlobalError } from './middleware';
@@ -19,6 +20,10 @@ app.use(cors(corsOptions));
 app.use('/api', express.static('public'));
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(cookieParser());
 
 app.use('/api/about', aboutRouter);
 app.use('/api/missions', missionRouter);
