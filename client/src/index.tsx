efb357aa91818dbb638c7fcafddf4396511f14e5
huyphamfc <huyphamfc@gmail.com>
2023-01-11 10:39:47 +0700

    setup Redux store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { GlobalStyles } from 'components';
 import App from 'App';
+import store from 'store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <GlobalStyles>
-    <App />
-  </GlobalStyles>,
+  <Provider store={store}>
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
+  </Provider>,
 );
